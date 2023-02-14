4ed7c821669c6d4564c49e243824504abe9d407a
Drew Pomatti <ampomatti@gmail.com>
2023-02-13 18:28:26 -0800

    setup routes and middleware

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
 import express from 'express';
+import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('hello from express')
-    res.status(200)
-    res.json({message: 'hello!'})
+app.use(cors())
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    req.secure;
+    next();
 })
+
+// app.get('/', (req, res) => {
+//     console.log('hello from express')
+//     res.status(200)
+//     res.json({ message: 'hello!' })
+// });
+
+app.use('/api', router);
 
 export default app;
